89f449f363dbd16900d2c1fd37e02552b984af3d
cbsim65535 <cbsim@zigbang.com>
2021-11-18 13:44:53 +0900

    u

--- driving/src/driving.py
+++ driving/src/driving.py
@@ -39,8 +39,8 @@ class RcBot:
     DRIVING_MIN_PULSE = 1228
     REVERSE_DRIVING_MIN_PULSE = 1228
 
-    ACCEL_SIZE_PER_ONE = (ESC_MAX_PULSE - DRIVING_MIN_PULSE) * 0.25
-    REVERSE_ACCEL_SIZE_PER_ONE = (REVERSE_DRIVING_MIN_PULSE - ESC_MIN_PULSE) * 0.5
+    ACCEL_SIZE_PER_ONE = (ESC_MAX_PULSE - DRIVING_MIN_PULSE) * 0.2
+    REVERSE_ACCEL_SIZE_PER_ONE = (REVERSE_DRIVING_MIN_PULSE - ESC_MIN_PULSE) * 0.4
 
     PULSE_SPEED_ZERO = 1228
 
@@ -83,7 +83,7 @@ class RcBot:
         if RcBot.ESC_MAX_PULSE < pulse:
             pulse = RcBot.PULSE_SPEED_ZERO
         try:
-            # rospy.loginfo("setEscPwm %f" % pulse)
+            rospy.loginfo("setEscPwm %f" % pulse)
             self.pwm.set_pwm(self.CH_ESC, 0, pulse)
         except:
             traceback.print_exc()
