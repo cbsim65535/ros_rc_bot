bee18fe1e0f0ae6ee45f8f3813b5785c08331d0e
cbsim65535 <cbsim@zigbang.com>
2022-06-23 14:28:45 +0900

    u

--- driving/test/test_pca9685_esc.py
+++ driving/test/test_pca9685_esc.py
@@ -11,14 +11,14 @@ import Adafruit_PCA9685
 
 
 # Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
 
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
 
 # Alternatively specify a different address and/or bus:
-#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+# pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
 # Configure min and max servo pulse lengths
 servo_min = 409  # Min pulse length out of 4096
@@ -28,11 +28,11 @@ servo_mid = 1638  # Max pulse length out of 4096
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
+print("Moving servo on channel 0, press Ctrl-C to quit...")
 while True:
     # Move servo on channel O between extremes.
-    i = input("0~1000 ?")
-    pulse = int(math.floor(float(i)/1000*4096))
+    i = input("400~600 ?")
+    pulse = i
     print(pulse)
-    pwm.set_pwm(1, 0, pulse)    
+    pwm.set_pwm(1, 0, pulse)
     time.sleep(1)
