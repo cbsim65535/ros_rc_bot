5ce7c76476231c47efa26ecbf71d90ea736ee57c
cbsim65535 <cbsim@zigbang.com>
2022-06-29 11:45:59 +0900

    u

--- driving/src/driving.py
+++ driving/src/driving.py
@@ -169,11 +169,7 @@ class RcBot:
             accel = RcBot.REVERSE_ACCEL_SIZE_PER_ONE / RcBot.ACCEL_STEP
         else:
             self.__speed = 0
-
-        if target_speed > self.__speed:
-            self.__speed += accel
-        if target_speed < self.__speed:
-            self.__speed += accel
+        self.__speed += accel
 
         if self.__speed < -RcBot.REVERSE_ACCEL_SIZE_PER_ONE:
             self.__speed = -RcBot.REVERSE_ACCEL_SIZE_PER_ONE
