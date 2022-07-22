979f1cd2b6712d39def7102da908a7f5a038b301
cbsim65535 <cbsim@zigbang.com>
2022-07-22 10:23:12 +0900

    u

--- basecam/setup.py
+++ basecam/setup.py
@@ -18,6 +18,6 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": ["run = basecam.basecam:main"],
+        "console_scripts": ["basecam = basecam.basecam:main"],
     },
 )
