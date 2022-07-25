400e6497532a3c99c3930b7be1eb5f53d0227534
cbsim65535 <cbsim@zigbang.com>
2022-07-25 14:54:14 +0900

    u

--- basecam/basecam/basecam.py
+++ basecam/basecam/basecam.py
@@ -694,7 +694,7 @@ class Basecam(Node):
             pass
 
     def byteJoin(self, b0, b1, b2):
-        b = str(bytes(b0, "ascii") + bytes(b1, "ascii") + b2)
+        b = bytes(b0, "ascii") + bytes(b1, "ascii") + b2
         return b
 
     def listen(self):
