a1e5c6008d547806879ffdc16591da224c281e1b
cbsim65535 <cbsim@zigbang.com>
2022-07-26 12:45:54 +0900

    u

--- bno055_node/setup.py
+++ bno055_node/setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 package_name = "bno055_node"
-submodules = "bno055"
 
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[package_name, submodules],
+    packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
