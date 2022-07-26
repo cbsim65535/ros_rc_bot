00cea23fc718164a0eec62179fadb9eaba336fa8
cbsim65535 <cbsim@zigbang.com>
2022-07-26 12:43:28 +0900

    u

--- bno055_node/setup.py
+++ bno055_node/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 package_name = "bno055_node"
+submodules = ["bno055"]
 
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[package_name],
+    packages=[package_name, submodules],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
@@ -18,6 +19,6 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": ["run = bno055_node.bno055_node:main"],
+        "console_scripts": ["bno055_node = bno055_node.bno055_node:main"],
     },
 )
