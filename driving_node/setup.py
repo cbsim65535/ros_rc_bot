5b810a702b73f23b35e6281848bd89b4c5a67b65
cbsim65535 <cbsim@zigbang.com>
2022-08-01 14:53:29 +0900

    u

--- driving_node/setup.py
+++ driving_node/setup.py
@@ -18,6 +18,6 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [],
+        "console_scripts": ["driving_node = driving_node.driving_node:main"],
     },
 )
