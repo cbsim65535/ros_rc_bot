8c5bd1b9eb1f11f272a80460256eb1a291e5866e
cbsim65535 <cbsim@zigbang.com>
2022-08-01 16:01:22 +0900

    u

--- driving_node/driving_node/driving_node.py
+++ driving_node/driving_node/driving_node.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-import rospy
+import rclpy
 import threading
 import traceback
-import time
-import math
 
+from rclpy.node import Node
 from pwm_msgs.msg import PwmCtrl
 from geometry_msgs.msg import TwistStamped
 
 
-class RcBot:
+class DrivingNode(Node):
 
     CH_SERVO = 0
     CH_ESC = 1
@@ -44,54 +43,53 @@ class RcBot:
     PULSE_SPEED_ZERO = 2048
 
     def __init__(self):
-        rospy.init_node("rc_car", anonymous=True)
+        super().__init__("driving_node")
 
         self.__speed = 0
-        self.__zoom = -RcBot.ZOOM_MIN_PULSE
+        self.__zoom = -DrivingNode.ZOOM_MIN_PULSE
 
-        rate = rospy.Rate(10)  # 10hz
+        self.rate = self.create_rate(10)
 
-        self.pub_pwm_set = rospy.Publisher("/pwm_ctrl/set", PwmCtrl, queue_size=10)
-
-        rospy.Subscriber("/cmd_vel", TwistStamped, self.on_cmd_vel)
-        # rospy.Subscriber("/camera/focus_ctrl", TwistStamped, self.on_focus_ctrl)
-
-        sec = 0
-        self.set_esc_pwm(RcBot.PULSE_SPEED_ZERO)
+        self.pub_pwm_set = self.create_publisher(PwmCtrl, "/pwm_ctrl/set", 10)
+        self.sub_cmd_vel = self.create_subscription(
+            TwistStamped, "/cmd_vel", self.on_cmd_vel
+        )
+        self.sub_cmd_vel  # prevent unused variable warning
+        self.set_esc_pwm(DrivingNode.PULSE_SPEED_ZERO)
 
     def set_esc_pwm(self, pulse):
         pulse = int(pulse)
-        if pulse < RcBot.PULSE_SPEED_ZERO:
-            if RcBot.REVERSE_DRIVING_MIN_PULSE < pulse:
-                pulse = RcBot.PULSE_SPEED_ZERO
-        if pulse > RcBot.PULSE_SPEED_ZERO:
-            if RcBot.DRIVING_MIN_PULSE > pulse:
-                pulse = RcBot.PULSE_SPEED_ZERO
-        if RcBot.ESC_MIN_PULSE > pulse:
-            pulse = RcBot.PULSE_SPEED_ZERO
-        if RcBot.ESC_MAX_PULSE < pulse:
-            pulse = RcBot.PULSE_SPEED_ZERO
+        if pulse < DrivingNode.PULSE_SPEED_ZERO:
+            if DrivingNode.REVERSE_DRIVING_MIN_PULSE < pulse:
+                pulse = DrivingNode.PULSE_SPEED_ZERO
+        if pulse > DrivingNode.PULSE_SPEED_ZERO:
+            if DrivingNode.DRIVING_MIN_PULSE > pulse:
+                pulse = DrivingNode.PULSE_SPEED_ZERO
+        if DrivingNode.ESC_MIN_PULSE > pulse:
+            pulse = DrivingNode.PULSE_SPEED_ZERO
+        if DrivingNode.ESC_MAX_PULSE < pulse:
+            pulse = DrivingNode.PULSE_SPEED_ZERO
 
         self.pub_pwm_set.publish(PwmCtrl(self.CH_ESC, pulse))
 
     def on_focus_ctrl(self, msg):
-        focus = msg.twist.angular.y * RcBot.FOCUS_SIZE_PER_ONE
-        if focus > RcBot.MAX_FOCUS_PULSE_SIZE:
-            focus = RcBot.MAX_FOCUS_PULSE_SIZE
-        if focus < -RcBot.MAX_FOCUS_PULSE_SIZE:
-            focus = -RcBot.MAX_FOCUS_PULSE_SIZE
+        focus = msg.twist.angular.y * DrivingNode.FOCUS_SIZE_PER_ONE
+        if focus > DrivingNode.MAX_FOCUS_PULSE_SIZE:
+            focus = DrivingNode.MAX_FOCUS_PULSE_SIZE
+        if focus < -DrivingNode.MAX_FOCUS_PULSE_SIZE:
+            focus = -DrivingNode.MAX_FOCUS_PULSE_SIZE
 
-        pulse = RcBot.FOCUS_CENTER_PULSE + focus
+        pulse = DrivingNode.FOCUS_CENTER_PULSE + focus
         self.set_focus_pwm(pulse)
 
-        zoom = self.__zoom - (msg.twist.angular.x * RcBot.ZOOM_SIZE_PER_ONE)
-        if zoom > RcBot.ZOOM_MAX_PULSE:
-            zoom = RcBot.ZOOM_MAX_PULSE
-        if zoom < RcBot.ZOOM_MIN_PULSE:
-            zoom = RcBot.ZOOM_MIN_PULSE
+        zoom = self.__zoom - (msg.twist.angular.x * DrivingNode.ZOOM_SIZE_PER_ONE)
+        if zoom > DrivingNode.ZOOM_MAX_PULSE:
+            zoom = DrivingNode.ZOOM_MAX_PULSE
+        if zoom < DrivingNode.ZOOM_MIN_PULSE:
+            zoom = DrivingNode.ZOOM_MIN_PULSE
         self.__zoom = zoom
 
-        pulse = RcBot.ZOOM_CENTER_PULSE + zoom
+        pulse = DrivingNode.ZOOM_CENTER_PULSE + zoom
         self.set_zoom_pwm(pulse)
         return
 
@@ -110,23 +108,23 @@ class RcBot:
     def on_cmd_vel(self, msg):
         # rospy.loginfo(msg)
 
-        steer = msg.twist.angular.z * RcBot.STEER_SIZE_PER_ONE
-        if steer > RcBot.MAX_STEER_PULSE_SIZE:
-            steer = RcBot.MAX_STEER_PULSE_SIZE
-        if steer < -RcBot.MAX_STEER_PULSE_SIZE:
-            steer = -RcBot.MAX_STEER_PULSE_SIZE
+        steer = msg.twist.angular.z * DrivingNode.STEER_SIZE_PER_ONE
+        if steer > DrivingNode.MAX_STEER_PULSE_SIZE:
+            steer = DrivingNode.MAX_STEER_PULSE_SIZE
+        if steer < -DrivingNode.MAX_STEER_PULSE_SIZE:
+            steer = -DrivingNode.MAX_STEER_PULSE_SIZE
 
-        pulse = RcBot.STEER_CENTER_PULSE + steer
+        pulse = DrivingNode.STEER_CENTER_PULSE + steer
         self.set_steer_pwm(pulse)
 
         target_speed = 0.0
         accel = 0.0
         if msg.twist.linear.x > 0:
-            target_speed = msg.twist.linear.x * RcBot.ACCEL_SIZE_PER_ONE
-            accel = RcBot.ACCEL_SIZE_PER_ONE / RcBot.ACCEL_STEP
+            target_speed = msg.twist.linear.x * DrivingNode.ACCEL_SIZE_PER_ONE
+            accel = DrivingNode.ACCEL_SIZE_PER_ONE / DrivingNode.ACCEL_STEP
         elif msg.twist.linear.x < 0:
-            target_speed = msg.twist.linear.x * RcBot.REVERSE_ACCEL_SIZE_PER_ONE
-            accel = RcBot.REVERSE_ACCEL_SIZE_PER_ONE / RcBot.ACCEL_STEP
+            target_speed = msg.twist.linear.x * DrivingNode.REVERSE_ACCEL_SIZE_PER_ONE
+            accel = DrivingNode.REVERSE_ACCEL_SIZE_PER_ONE / DrivingNode.ACCEL_STEP
         else:
             self.__speed = 0.0
 
@@ -135,28 +133,26 @@ class RcBot:
         if target_speed < self.__speed:
             self.__speed -= accel
 
-        pulse = RcBot.PULSE_SPEED_ZERO
-        if self.__speed < -RcBot.REVERSE_ACCEL_SIZE_PER_ONE:
-            self.__speed = -RcBot.REVERSE_ACCEL_SIZE_PER_ONE
-        if self.__speed > RcBot.ACCEL_SIZE_PER_ONE:
-            self.__speed = RcBot.ACCEL_SIZE_PER_ONE
+        pulse = DrivingNode.PULSE_SPEED_ZERO
+        if self.__speed < -DrivingNode.REVERSE_ACCEL_SIZE_PER_ONE:
+            self.__speed = -DrivingNode.REVERSE_ACCEL_SIZE_PER_ONE
+        if self.__speed > DrivingNode.ACCEL_SIZE_PER_ONE:
+            self.__speed = DrivingNode.ACCEL_SIZE_PER_ONE
         if self.__speed < 0.0:
-            pulse = RcBot.REVERSE_DRIVING_MIN_PULSE + self.__speed
+            pulse = DrivingNode.REVERSE_DRIVING_MIN_PULSE + self.__speed
         if self.__speed > 0.0:
-            pulse = RcBot.DRIVING_MIN_PULSE + self.__speed
+            pulse = DrivingNode.DRIVING_MIN_PULSE + self.__speed
 
         self.set_esc_pwm(pulse)
 
-    def stop(self):
-        self.__is_loop = False
+
+def main(args=None):
+    rclpy.init(args=args)
+    driving = DrivingNode()
+    rclpy.spin(driving)
+    driving.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
-    try:
-        rcbot = RcBot()
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        traceback.print_exc()
-    finally:
-        traceback.print_exc()
-        rcbot.stop()
+    main()
