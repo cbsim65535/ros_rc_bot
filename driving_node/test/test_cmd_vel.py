28c6df4936b895a478b8b0eb438a6a8ec5f38d69
cbsim65535 <cbsim@zigbang.com>
2022-08-03 10:47:31 +0900

    u

--- driving_node/test/test_cmd_vel.py
+++ driving_node/test/test_cmd_vel.py
@@ -16,37 +16,39 @@ from basecam_msgs.srv import (
 )
 
 
-class TestCmdVel(Node):
+class TestCamera(Node):
     def __init__(self):
-        super().__init__("test_cmd_vel_node")
+        super().__init__("test_camera_node")
 
         self.rate = self.create_rate(10)
-        self.pub_cmd_vel = self.create_publisher(TwistStamped, "/cmd_vel", 10)
-        x = 0
-        dx = 1
+        self.pub_camera_ctrl = self.create_publisher(
+            TwistStamped, "/basecam/direct_ctrl", 10
+        )
+        y = 0
+        dy = 1
         z = 0
         dz = 1
 
         while rclpy.ok():
             twist_stapmed = TwistStamped()
             twist_stapmed.header.stamp = self.get_clock().now().to_msg()
-            twist_stapmed.twist.linear.x = x
+            twist_stapmed.twist.angular.y = y
             twist_stapmed.twist.angular.z = z
-            if x >= 0.5:
-                dx = -1
-            if x <= 0.5:
-                dx = 1
+            if y >= 0.5:
+                dy = -1
+            if y <= 0.5:
+                dy = 1
             if z >= 0.5:
                 dz = -1
             if z <= 0.5:
                 dz = 1
-            x += 0.1 * dx
+            y += 0.1 * dy
             z += 0.1 * dz
 
 
 def main(args=None):
     rclpy.init(args=args)
-    manual = TestCmdVel()
+    manual = TestCamera()
     rclpy.spin(manual)
     manual.destroy_node()
     rclpy.shutdown()
