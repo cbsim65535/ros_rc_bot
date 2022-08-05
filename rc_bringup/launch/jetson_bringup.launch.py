0a7c75b66069c21598314536bc4181ce19d1bc6b
cbsim65535 <cbsim@zigbang.com>
2022-08-05 16:59:00 +0900

    u

--- rc_bringup/launch/jetson_bringup.launch.py
+++ rc_bringup/launch/jetson_bringup.launch.py
@@ -29,33 +29,37 @@ def generate_launch_description():
     share_dir = get_package_share_directory("rc_bringup")
     parameter_file = LaunchConfiguration("params_file")
     node_name = "ydlidar_ros2_driver_node"
-
-    params_declare1 = DeclareLaunchArgument(
-        "params_file",
-        default_value=os.path.join(share_dir, "config", "ydlidar1.yaml"),
-        description="FPath to the ROS2 parameters file to use.",
-    )
-
-    driver_node1 = LifecycleNode(
-        package="ydlidar_ros2_driver",
-        executable="ydlidar_ros2_driver_node",
-        name="ydlidar_ros2_driver_node1",
-        output="screen",
-        emulate_tty=True,
-        parameters=[parameter_file],
-        namespace="/",
-    )
-    tf2_node1 = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        name="static_tf_pub_laser1",
-        arguments=["0", "0", "0.02", "0", "0", "0", "1", "base_link", "laser_frame"],
-    )
-
     return LaunchDescription(
         [
-            params_declare1,
-            driver_node1,
-            tf2_node1,
+            DeclareLaunchArgument(
+                "params_file",
+                default_value=os.path.join(share_dir, "config", "ydlidar1.yaml"),
+                description="FPath to the ROS2 parameters file to use.",
+            ),
+            LifecycleNode(
+                package="ydlidar_ros2_driver",
+                executable="ydlidar_ros2_driver_node",
+                name="ydlidar_ros2_driver_node1",
+                output="screen",
+                emulate_tty=True,
+                parameters=[parameter_file],
+                namespace="/",
+            ),
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                name="static_tf_pub_laser1",
+                arguments=[
+                    "0",
+                    "0",
+                    "0.02",
+                    "0",
+                    "0",
+                    "0",
+                    "1",
+                    "base_link",
+                    "laser_frame",
+                ],
+            ),
         ]
     )
