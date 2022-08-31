8b724e8dab24e419d4d848bd8668d74c29b32d06
cbsim65535 <cbsim@zigbang.com>
2022-08-31 18:33:22 +0900

    u

--- odom_node/setup.py
+++ odom_node/setup.py
@@ -23,6 +23,5 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": ["odom_node = odom_node.odom_node:main"],
-        "console_scripts": ["odom_node = odom_node.ls7366r:main"],
     },
 )
